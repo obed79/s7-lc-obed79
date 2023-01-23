92a2cabf144e3f8e3c95f4625c3313431d5c664c
obed79 <obedgyawu8@gmail.com>
2023-01-23 01:18:43 -0500

    changes

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -8,7 +8,8 @@ public class LoanCalculator {
      * TODO: finish out this JavaDoc comment block.
      * FIXME: WHAT DOES THIS METHOD DO?
      * the code is calculating the amount that needed to pay the loan with 10% of the remaining balance
-     * 
+     * the for loop is to loop through the first months to the third and then add the 10% to the remaining-amount when been check by the for loop
+     * the return amount - return the remaining amount after the calculation has been made the for loop
      * @param ???
      * @return ???
      */
